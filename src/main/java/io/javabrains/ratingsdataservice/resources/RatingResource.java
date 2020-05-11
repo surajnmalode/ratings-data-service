647b60328db0583bd61d0514d61ec863c2aea47f
surajnmalode <surajnmalode@gmail.com>
2020-05-11 20:26:00 +0530

    Code committed and pushed by surajnmalode@gmail.com on 11-May-2020 08:22 PM using DDLAB Gitpusher tool

---
+++ src/main/java/io/javabrains/ratingsdataservice/resources/RatingResource.java
@@ -0,0 +1,27 @@
+package io.javabrains.ratingsdataservice.resources;
+
+import io.javabrains.ratingsdataservice.models.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.javabrains.ratingsdataservice.models.Rating;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratingsData")
+public class RatingResource {
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+}
